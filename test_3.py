fb2aba2d9a7c7bc73bc20d667f7b68d437b80c12
13509 <1350962725@qq.com>
2024-09-17 23:31:31 +0800

    增加了无参数装饰器

--- test_3.py
+++ test_3.py
@@ -6,9 +6,25 @@ def repeat(times):
         return call_helper
     return repeat_helper
 
+import time
+def calc_time(func):
+    def wapper_calc(*args):
+        start = time.time()
+        func(*args)
+        end = time.time()
+        cost_time = end - start
+        print("function名字"+func.__name__ +"共耗时"+str(cost_time))
+    return wapper_calc
+
+
 @repeat(5)
 def test_w(name):
     print(name)
+@calc_time
+def test_w2(name):
+    time.sleep(2)
+    print(name)
 
 if __name__ == "__main__":
-    test_w("wjc")
+    # test_w("wjc")
+    test_w2("wjc")
